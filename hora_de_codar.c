7ba04e2330f230128a5c1084332a50c8eb4d05c2
Thiago Gabriel <150717336+thiagoed-souza@users.noreply.github.com>
2026-02-22 09:00:43 -0300

    Add files via upload

---
+++ hora_de_codar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int idade, matricula;
+    float altura;
+    char nome[50];
+
+
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    printf("Digite sua altura: ");
+    scanf("%f", &altura);
+
+    printf("Digite seu nome: ");
+    scanf("%s", nome);
+
+    printf("Digite sua matrícula: ");
+    scanf("%d", &matricula);
+
+    printf("Nome do aluno: %s - Matrícula: %d", nome, matricula);
+    printf("Idade: %d - Altura: %f", idade, altura);
+
+    return 0;
+
+}
